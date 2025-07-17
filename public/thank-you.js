ed05b6900fb67ea0340f6804619afd1d8d418af8
cutiss <seasocutis@gmail.com>
2025-07-17 18:20:47 +0100

    Update thank-you.js

--- public/thank-you.js
+++ public/thank-you.js
@@ -1,1 +1,11 @@
 
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const bgImage = new Image();
+bgImage.src = 'background1.png'; // For thank-you page background
+
+bgImage.onload = () => {
+  canvas.width = bgImage.width;
+  canvas.height = bgImage.height;
+  ctx.drawImage(bgImage, 0, 0);
+};
